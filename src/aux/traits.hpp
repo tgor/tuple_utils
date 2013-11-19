4d8f2079b7eb6bcebc1d0d130f4021f33c820bf1
tgor <tstepniak21@gmail.com>
2013-11-19 20:09:01 +0100

    added result_of_rec trait

--- src/aux/traits.hpp
+++ src/aux/traits.hpp
@@ -15,8 +15,8 @@ namespace tuple_utils
 {
 
 /**
- *@brief Traits for getting one common type from a list of types
- *Internally uses and works like std::common_type, but allows variable number of parameters
+ * @brief Traits for getting one common type from a list of types
+ * Internally uses and works like std::common_type, but allows variable number of parameters
  */
 template <
         typename T,
@@ -28,7 +28,7 @@ struct common_type_var : private static_
 };
 
 /**
- *@brief Last step of recursive common_type_var<>::type, for two types gives simple std::common_type<>::type
+ * @brief Last step of recursive common_type_var<>::type, for two types gives simple std::common_type<>::type
  */
 template <
         typename T,
@@ -40,7 +40,7 @@ struct common_type_var<T, Y> : private static_
 };
 
 /**
- *@brief Special case when common_type_var is parameterized with only one type
+ * @brief Special case when common_type_var is parameterized with only one type
  */
 template <
         typename T
@@ -51,7 +51,7 @@ struct common_type_var<T> : private static_
 };
 
 /**
- *@brief Obtains smallest size for the number of given tuples
+ * @brief Obtains smallest size for the number of given tuples
  */
 template <typename T, typename... Args>
 struct tsize_min : private static_
@@ -61,7 +61,7 @@ struct tsize_min : private static_
 };
 
 /**
- *@brief Last step of recursive tsize_min<>::value, value is equal to the size of smaller tuple
+ * @brief Last step of recursive tsize_min<>::value, value is equal to the size of smaller tuple
  */
 template <typename T, typename Y>
 struct tsize_min<T, Y> : private static_
@@ -71,12 +71,92 @@ struct tsize_min<T, Y> : private static_
 };
 
 /**
- *@brief Special case when tsize_min is parameterized with only one type
- *Derives from std::integral_constant to get value member of type std::size_t with value equal to std::tuple_size<>::value
+ * @brief Special case when tsize_min is parameterized with only one type
+ * Derives from std::integral_constant to get value member of type std::size_t with value equal to std::tuple_size<>::value
  */
 template <typename T>
 struct tsize_min<T> : std::integral_constant<std::size_t, std::tuple_size<T>::value>, private static_
 { };
+
+template <typename T, typename... Args>
+struct tsize_min
+{
+ using CT = typename std::decay<T>::type;
+ static constexpr std::size_t value = std::tuple_size<CT>::value < tsize_min<Args...>::value
+ ? std::tuple_size<CT>::value : tsize_min<Args...>::value;
+};
+
+/**
+ * @brief Last step of recursive tsize_min<>::value, value is equal to the size of smaller tuple
+ */
+template <typename T, typename Y>
+ struct tsize_min<T, Y>
+{
+ using CT = typename std::decay<T>::type;
+ using CY = typename std::decay<Y>::type;
+ static constexpr std::size_t value = std::tuple_size<CT>::value < std::tuple_size<CY>::value
+ ? std::tuple_size<CT>::value : std::tuple_size<CY>::value;
+};
+
+/**
+ * @brief Special case when tsize_min is parameterized with only one type
+ * Derives from std::integral_constant to get value member of type std::size_t with value equal to std::tuple_size<>::value
+ */
+template <typename T>
+struct tsize_min<T> : std::integral_constant<std::size_t, std::tuple_size<typename std::decay<T>::type>::value>
+{ };
+
+//Forward declaration
+template <typename...>
+struct result_of_rec;
+
+/**
+ * @brief Trait used to get return value of a recursive binary function call f(x, f(y, ...f(z1, fz2)))
+ * Function f could take it's arguments by value, reference or both
+ * @tparam FuncType - function type, could be function pointer, lambda or functor
+ * @tparam F - first argument
+ * @tparam R... - rest of the arguments, used in recursive call
+ */
+template <
+        typename FuncType,
+        typename F,
+        typename... R
+        >
+struct result_of_rec<FuncType, F, R...>
+{
+    using type = decltype(std::declval<FuncType>()(
+        std::declval<typename std::add_lvalue_reference<F>::type>(),
+        std::declval<typename std::add_lvalue_reference<typename result_of_rec<FuncType, R...>::type>::type>()
+    ));
+};
+
+/**
+ * @brief Last step of result_of_rec<FuncType, F, R...>, type is set to the return value of a binary function f(x,y)
+ */
+template <
+        typename FuncType,
+        typename T,
+        typename Y
+        >
+struct result_of_rec<FuncType, T, Y>
+{
+    using type = decltype(std::declval<FuncType>()(
+        std::declval<typename std::add_lvalue_reference<T>::type>(),
+        std::declval<typename std::add_lvalue_reference<Y>::type>()
+    ));
+};
+
+/**
+ * @brief Get return value of unary function f(x)
+ */
+template <
+        typename FuncType,
+        typename T
+        >
+struct result_of_rec<FuncType, T>
+{
+    using type = decltype(std::declval<FuncType>()(std::declval<typename std::add_lvalue_reference<T>::type>()));
+};
 
 } // namespace tuple_utils
 
