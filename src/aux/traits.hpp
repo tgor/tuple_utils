f0f9b8d73abfdda5800d3302bd854d81a4b06fdd
tgor <tstepniak21@gmail.com>
2013-11-19 22:33:59 +0100

    fixed behaviour for tsize_min

--- src/aux/traits.hpp
+++ src/aux/traits.hpp
@@ -56,8 +56,9 @@ struct common_type_var<T> : private static_
 template <typename T, typename... Args>
 struct tsize_min : private static_
 {
-    static constexpr std::size_t value = std::tuple_size<T>::value < tsize_min<Args...>::value
-        ? std::tuple_size<T>::value : tsize_min<Args...>::value;
+    using CT = typename std::decay<T>::type;
+    static constexpr std::size_t value = std::tuple_size<CT>::value < tsize_min<Args...>::value
+        ? std::tuple_size<CT>::value : tsize_min<Args...>::value;
 };
 
 /**
@@ -66,8 +67,10 @@ struct tsize_min : private static_
 template <typename T, typename Y>
 struct tsize_min<T, Y> : private static_
 {
-    static constexpr std::size_t value = std::tuple_size<T>::value < std::tuple_size<Y>::value
-        ? std::tuple_size<T>::value : std::tuple_size<Y>::value;
+    using CT = typename std::decay<T>::type;
+    using CY = typename std::decay<Y>::type;
+    static constexpr std::size_t value = std::tuple_size<CT>::value < std::tuple_size<CY>::value
+        ? std::tuple_size<CT>::value : std::tuple_size<CY>::value;
 };
 
 /**
@@ -75,35 +78,9 @@ struct tsize_min<T, Y> : private static_
  * Derives from std::integral_constant to get value member of type std::size_t with value equal to std::tuple_size<>::value
  */
 template <typename T>
-struct tsize_min<T> : std::integral_constant<std::size_t, std::tuple_size<T>::value>, private static_
-{ };
-
-template <typename T, typename... Args>
-struct tsize_min
-{
- using CT = typename std::decay<T>::type;
- static constexpr std::size_t value = std::tuple_size<CT>::value < tsize_min<Args...>::value
- ? std::tuple_size<CT>::value : tsize_min<Args...>::value;
-};
-
-/**
- * @brief Last step of recursive tsize_min<>::value, value is equal to the size of smaller tuple
- */
-template <typename T, typename Y>
- struct tsize_min<T, Y>
-{
- using CT = typename std::decay<T>::type;
- using CY = typename std::decay<Y>::type;
- static constexpr std::size_t value = std::tuple_size<CT>::value < std::tuple_size<CY>::value
- ? std::tuple_size<CT>::value : std::tuple_size<CY>::value;
-};
-
-/**
- * @brief Special case when tsize_min is parameterized with only one type
- * Derives from std::integral_constant to get value member of type std::size_t with value equal to std::tuple_size<>::value
- */
-template <typename T>
-struct tsize_min<T> : std::integral_constant<std::size_t, std::tuple_size<typename std::decay<T>::type>::value>
+struct tsize_min<T> :
+        std::integral_constant<std::size_t, std::tuple_size<typename std::decay<T>::type>::value>,
+        private static_
 { };
 
 //Forward declaration
